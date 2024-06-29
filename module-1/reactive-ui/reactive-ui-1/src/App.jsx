257cefcf7f55176c98acbcaaf9557b66f8502a50
Montasir Azad <montasirazad@gmail.com>
2024-06-30 03:56:17 +0600

    completed counter app

--- module-1/reactive-ui/reactive-ui-1/src/App.jsx
+++ module-1/reactive-ui/reactive-ui-1/src/App.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import Stats from "./components/Stats";
+
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  },
+];
 
 function App() {
+  const [state, setState] = useState(initialState);
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+  };
+  const increment = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        };
+      }
+      return { ...c };
+    });
+    setState(updatedCounter);
+  };
+  const decrement = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count - 1,
+        };
+      }
+      return { ...c };
+    });
+    setState(updatedCounter);
+  };
   return (
     <>
       <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -10,9 +51,17 @@ function App() {
         </h1>
 
         <div className="max-w-md mx-auto mt-10 space-y-5">
-          <Counter />
-          <Counter />
-          <Counter />
+          {state.map((count) => (
+            <Counter
+            count={count.count}
+              key={count.id}
+              onIncrement={increment}
+              onDecrement={decrement}
+              id={count.id}
+            />
+          ))}
+
+          <Stats count={totalCount()} />
         </div>
       </div>
     </>
---
+++ module-1/reactive-ui/reactive-ui-1/src/components/Count.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const Count = ({count}) => {
+  return (
+    <div className="text-2xl font-semibold" id="counter">
+      {count}
+    </div>
+  );
+};
+
+export default Count;
